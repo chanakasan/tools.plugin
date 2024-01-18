ab97a0d8b967472b4813a53c2c89cb91af1c27f6
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-01-18 09:23:35 +0530

    rev

--- bash/main.sh
+++ bash/main.sh
@@ -2,6 +2,6 @@ for f in $nex_mods_path/bash/*.bash; do
   source $f
 done
 
-for f in $HOME/user_mods/*.mod; do
+for f in $HOME/user_mods/*.bash; do
   source $f
 done
--- setup/install.sh
+++ setup/install.sh
@@ -1,5 +1,44 @@
+#!/bin/bash
 
-#__nex_mods_start
-export nex_mods_path=$HOME/nex-mods
-source $nex_mods_path/lib/main.sh
-#__nex_mods_end
+set -e
+
+main() {
+  local title="Nex Mods"
+  local dir_name="nex-mods"
+  local start_text='__nex_mods_start'
+  local end_text='__nex_mods_end'
+  local bashrc="$HOME/.bashrc"
+  local nex_mods_path=$(get_root_path)/$dir_name
+  echo " Installing - $title"
+  remove_from_bashrc
+  copy_to_bashrc
+  echo ""
+  echo " done"
+  echo ""
+}
+
+get_root_path() {
+  local user=$(whoami)
+  if [ "$user" = "codespace" ]; then
+    echo /workspaces/.codespaces/.persistedshare
+  else
+    echo $HOME/dotfiles
+  fi
+}
+
+remove_from_bashrc() {
+  sed -i '/#'$start_text'/,/#'$end_text'/{d}' $bashrc
+}
+
+copy_to_bashrc() {
+  echo "" >> $bashrc
+  echo "#$start_text" >> $bashrc
+  echo 'export nex_mods_path='$nex_mods_path >> $bashrc
+  echo 'source $nex_mods_path/bash/main.sh' >> $bashrc
+  echo 'export PATH=$nex_mods_path/bin:$PATH' >> $bashrc
+  echo "#$end_text" >> $bashrc
+  echo "" >> $bashrc
+}
+
+# _end_
+main
