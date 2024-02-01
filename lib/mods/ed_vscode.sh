e826f3e03d30d8e5472da0a498676f7fcb742ca1
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-02-01 10:56:12 +0530

    rev

--- lib/mods/ed_vscode.sh
+++ lib/mods/ed_vscode.sh
@@ -7,10 +7,6 @@ _ed_cli() {
   vscode $HOME/dotfiles/nex-cli
 }
 
-_ed_dev() {
-  vscode $nex_devscript_path
-}
-
 _ed_bash() {
   vscode $nex_bash_path
 }
@@ -28,11 +24,17 @@ _ed_mod() {
 }
 
 _ed_git() {
-  vscode $nex_devscript_path
+  local nex_git_path=$HOME/dotfiles/nex-git
+  vscode $nex_git_path
 }
 
 _ed_script() {
-  vscode $nex_script_path
+  local nex_devscript_path=$HOME/dotfiles/devscript
+  vscode $nex_devscript_path
+}
+
+_ed_modules() {
+  vscode $HOME/wip/lib/base-modules
 }
 
 _ed_tpl() {
